4a3ffe09f61c945019f64e00704d46f7f30ec5ce
stmaire <stmaire@student.42lyon.fr>
2026-09-21 17:40:17 +0200

    docker-compose added

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
+
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql > /dev/null
+
+    mysqld --user=mysql &
+    pid=$!
+
+    until mysqladmin ping --silent; do
+        sleep 1
+    done
+
+    mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+    mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'\%' IDENTIFIED BY '${SQL_PASSWORD}';"
+    mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%';"
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
+    mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
+    wait $pid
 fi
 
-mysqld --user=mysql &
-
-until mysqladmin ping --silent; do
-    sleep 1
-done
-
-mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
-mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-
-mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
-
-exec mysqld
+exec mysqld --user=mysql
--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
-sleep 10
+until mysqladmin -h mariadb -u"$SQL_USER" -p"$SQL_PASSWORD" ping --silent 2>/dev/null; do
+    sleep 1
+done
 
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
+
     wp config create \
         --allow-root \
-        --dbname=$SQL_DATABASE \
-        --dbuser=$SQL_USER \
-        --dbpass=$SQL_PASSWORD \
-        --dbhost=mariadb:3306 \
+        --dbname="$SQL_DATABASE" \
+        --dbuser="$SQL_USER" \
+        --dbpass="$SQL_PASSWORD" \
+        --dbhost="mariadb:3306" \
         --path='/var/www/wordpress'
 
     wp core install \
         --allow-root \
-        --url=$DOMAIN_NAME \
-        --title=$WP_TITLE \
-        --admin_user=$WP_ADMIN_USER \
-        --admin_password=$WP_ADMIN_PASSWORD \
-        --admin_email=$WP_ADMIN_EMAIL \
+        --url="$DOMAIN_NAME" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN_USER" \
+        --admin_password="$WP_ADMIN_PASSWORD" \
+        --admin_email="$WP_ADMIN_EMAIL" \
         --path='/var/www/wordpress'
 
     wp user create \
         --allow-root \
-        $WP_USER $WP_EMAIL \
-        --user_pass=$WP_PASSWORD \
+        "$WP_USER" "$WP_EMAIL" \
+        --user_pass="$WP_PASSWORD" \
         --role=author \
         --path='/var/www/wordpress'
+
+    chown -R www-data:www-data /var/www/wordpress
 fi
 
 exec /usr/sbin/php-fpm8.2 -F
