75882e23ff640060d6e808b08909fa3fb49ec868
stmaire <stmaire@student.42lyon.fr>
2026-09-23 14:39:41 +0200

    mariadb and wordpress connected

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
+set -e
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql > /dev/null
-
-    mysqld --user=mysql &
-    pid=$!
-
-    until mysqladmin ping --silent; do
-        sleep 1
-    done
-
-    mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-    mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"    mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%';"
-    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-
-    mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
-    wait $pid
+    mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql >/dev/null 2>&1
 fi
+
+mysqld --user=mysql --skip-networking &
+pid=$!
+
+for i in $(seq 1 30); do
+    if mysqladmin ping --silent >/dev/null 2>&1; then
+        break
+    fi
+    sleep 1
+done
+
+if mysqladmin -uroot -p"${SQL_ROOT_PASSWORD}" ping --silent >/dev/null 2>&1; then
+    ROOT_AUTH=(-uroot -p"${SQL_ROOT_PASSWORD}")
+else
+    ROOT_AUTH=(-uroot)
+fi
+
+mysql "${ROOT_AUTH[@]}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql "${ROOT_AUTH[@]}" -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql "${ROOT_AUTH[@]}" -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%';"
+mysql "${ROOT_AUTH[@]}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql "${ROOT_AUTH[@]}" -e "FLUSH PRIVILEGES;"
+
+mysqladmin -uroot -p"${SQL_ROOT_PASSWORD}" shutdown
+wait $pid
 
 exec mysqld --user=mysql
